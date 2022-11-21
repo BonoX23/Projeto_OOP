4eae91404809b75f1c801c949e7599c78f690f89
Ricardo Bono <83359117+BonoX23@users.noreply.github.com>
2022-11-21 16:27:18 -0300

    Iniciando interação

--- Heranca/Heranca/Program.cs
+++ Heranca/Heranca/Program.cs
@@ -7,41 +7,54 @@ namespace Heranca.Entidades
     {
         static void Main(string[] args)
         {
-            Conta c1 = new Conta(1001, "Alex", 500.00);
+            List<Conta> list = new List<Conta>();
 
-            Conta c2 = new ContaPoupanca(1002, "Ana", 500.00, 0.02);
+            Console.WriteLine("Quantas contas deseja cadastrar?");
+            int n = int.Parse(Console.ReadLine());
 
-            Conta c3 = new ContaEmpresarial(1003, "Roberto", 200.00, 1000.00);
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine("Conta Corrente, Poupança ou Empresarial (digitar c / p / e)?");
+                char type = char.Parse(Console.ReadLine());
+                Console.WriteLine("Qual o numero da conta?");
+                int numero = int.Parse(Console.ReadLine());
+                Console.WriteLine("Qual o nome do titular?");
+                string titular = Console.ReadLine();
+                Console.WriteLine("Qual o saldo inicial? ex: 0.00");
+                double saldo = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Saldo inicial da conta do Alex");
-            Console.WriteLine(c1.Saldo);
-            Console.WriteLine();
-            Console.WriteLine("Saldo inicial da conta da Ana");
-            Console.WriteLine(c2.Saldo);
-            Console.WriteLine();
-            Console.WriteLine("Saldo inicial da conta do Roberto");
-            Console.WriteLine(c3.Saldo);
-            Console.WriteLine();
+                switch (type)
+                {
+                    case 'c':
+                        {
+                            list.Add(new Conta(numero, titular, saldo));
+                        };
+                        break;
 
-            //Aplicando a retida de R$ 10,00 na conta do Alex e da Ana
-            c1.Retirada(10.00);
-            c2.Retirada(10.00);
+                    case 'p':
+                        {
+                            Console.WriteLine("Qual a taxa de juros?");
+                            double taxaDeJuros = double.Parse(Console.ReadLine());
+                            list.Add(new ContaPoupanca(numero, titular, saldo, taxaDeJuros));
+                        };
+                        break;
 
-            //Aplicando um emprestimo de R$ 100,00 na conta do Roberto
-            c3.Emprestimo(100.00);
+                    case 'e':
+                        {
+                            Console.WriteLine("Qual o limite de crédito?");
+                            double limiteDeEmprestimo = double.Parse(Console.ReadLine());
+                            list.Add(new ContaEmpresarial(numero, titular, saldo, limiteDeEmprestimo));
+                        };
+                        break;
 
-            Console.WriteLine("Saldo atualizado da conta do Alex, após a retirada");
-            Console.WriteLine(c1.Saldo);
-            Console.WriteLine();
-            Console.WriteLine("Saldo atualizado da conta da Ana , após a retirada");
-            Console.WriteLine(c2.Saldo);
-            Console.WriteLine();
-            Console.WriteLine("Saldo atualizado da conta do Roberto , após ao acrescimo do emprestimo");
-            Console.WriteLine(c3.Saldo);
+                    default:
+                        {
+                            Console.WriteLine("Favor digitar c / p / e) para um tipo de conta");
+                        };
+                        break;
+                }
 
-
-
-
+            }
         }
     }
 }
